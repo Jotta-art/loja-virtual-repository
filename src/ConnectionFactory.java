91167163cc0678d382688ae2bcd0a1c55eb0f2e4
João Pedro <joao.pedro42@outlook.com.br>
2021-03-23 14:09:13 -0300

    Alterando a forma de criar conexões utilizando o pool de conexões

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,13 +1,25 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
 
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        comboPooledDataSource.setUser("qa_homo");
+        comboPooledDataSource.setPassword("qa_homo");
+
+        comboPooledDataSource.setMaxPoolSize(15);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection criaConexao() throws SQLException {
-        return DriverManager
-                .getConnection
-                        ("jdbc:oracle:thin:@localhost:1521:XE",
-                                "qa_homo", "qa_homo");
+        return this.dataSource.getConnection();
     }
 }

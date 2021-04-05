e0e7e82aefabe51f693a9319f854278491a236df
João Pedro <joao.pedro42@outlook.com.br>
2021-04-05 16:47:16 -0300

    Refatorando o código para evitar queries N + 1

--- src/br/com/alura/jdbc/TestaListagemDeCategorias.java
+++ src/br/com/alura/jdbc/TestaListagemDeCategorias.java
@@ -2,6 +2,7 @@ package br.com.alura.jdbc;
 
 import br.com.alura.jdbc.dao.CategoriaDao;
 import br.com.alura.jdbc.modelo.Categoria;
+import br.com.alura.jdbc.modelo.Produto;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,8 +14,14 @@ public class TestaListagemDeCategorias {
 
         try (Connection connection = new ConnectionFactory().criaConexao()) {
             CategoriaDao categoriaDao = new CategoriaDao(connection);
-            List<Categoria> listaDeCategorias = categoriaDao.listar();
-            listaDeCategorias.stream().forEach(ct -> System.out.println(ct.getNome()));
+            List<Categoria> listaDeCategorias = categoriaDao.listarComProdutos();
+            listaDeCategorias.stream().forEach(ct -> {
+                System.out.println(ct.getNome());
+                for (Produto produto : ct.getProdutos()) {
+                    System.out.println(ct.getNome() + " - " + produto.getNome());
+
+                }
+            });
         }
     }
 }
